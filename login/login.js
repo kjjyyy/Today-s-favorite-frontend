8fc0e3fbd9f2247032e13061fd2ddd6d51d774cd
KimJjeongYyun <161802787+KimJjeongYyun@users.noreply.github.com>
2024-07-16 13:59:55 +0900

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,6 @@
+function loginSuccess(event) {
+  event.preventDefault();
+  alert("로그인 성공!");
+  window.location.href =
+    "http://127.0.0.1:5500/%EC%98%A4%EB%8A%98%EC%9D%98%20%EC%B5%9C%EC%95%A0/main_IU/main_IU.html";
+}
